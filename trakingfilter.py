be0c02f8231916682f76af02168342185b6e1dfb
NU <a175@users.noreply.github.com>
2017-05-29 06:01:06 +0900

    Implemented: bg, template.

---
+++ trakingfilter.py
@@ -0,0 +1,255 @@
+import numpy
+import cv2
+
+class TrackingFilter:
+    def __init__(self,original):
+        self.original = original
+        cv2.namedWindow("frame", cv2.CV_WINDOW_AUTOSIZE)
+        self.mouse_event_note={}
+        self.mouse_event_result=[]
+        self.direction=0
+        (ret, self.frame) = self.original.read()
+        self.bg=[]
+        self.template=[]
+        self.features=None
+        self.CRITERIA = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
+        
+    def select_background(self):
+        print "#","1. choose backgroud frames."
+        print "#","x: append the frame as bg;"
+        self.event_x=self.append_current_frame_to_bg
+        self.currentstep=100
+        self.update_frame(0)
+        self.showvideo()
+
+    def select_template(self):
+        print "#","2. choose target unit."
+        print "#","mouse drag: append the rectangle;"
+        self.event_x=None
+        self.inputted_data=[]
+        cv2.setMouseCallback("frame", self.select_rectangle_by_mouse)
+        self.currentstep=200
+        self.update_frame(0)
+        self.showvideo()
+        self.template=[]
+        for (pos,xp,x,yp,y) in self.inputted_data:
+            if yp>y:
+                t=yp
+                yp=y
+                y=t
+            if xp>x:
+                t=xp
+                xp=x
+                x=t
+            self.original.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos)
+            (ret, frame) = self.original.read()
+            cv2.imshow('template',frame[yp:y,xp:x])
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            self.template.append(gray[yp:y,xp:x])
+
+    def select_feature(self):
+        print "#","3. choose target unit."
+        print "#","mouse click: select the particle;"
+        self.event_x=None
+        self.inputted_data=[]
+        cv2.setMouseCallback("frame", self.select_point_by_mouse)
+        self.currentstep=300
+        self.update_frame(0)
+        self.showvideo()
+        
+    def run(self):
+        self.select_background()
+        self.select_template()
+        self.select_feature()
+
+
+    def append_current_frame_to_bg(self):
+        self.bg.append(numpy.copy(self.frame))
+        pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+        print "append", pos, "to bg."
+
+
+    def append_selected_point_to_feature(self,x,y):
+        if self.features is None:
+            self.features = numpy.array([[[x, y]]], numpy.float32)
+            self.status = numpy.array([1])
+
+        else:
+            self.features = numpy.append(self.features, [[[x, y]]], axis = 0).astype(numpy.float32)
+            self.status = numpy.append(self.status, 1)
+
+        cv2.cornerSubPix(self.frame, self.features, (10, 10), (-1, -1), self.CRITERIA)
+                                                                        
+    def select_rectangle_by_mouse(self,event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONUP:
+            (xp,yp)=self.mouse_event_note["LBUTTONDOWN"]
+            self.direction=self.mouse_event_note["direction"]
+            self.frame=numpy.copy(self.mouse_event_note["frame"])
+            self.mouse_event_note={}
+            pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)-1
+            self.inputted_data.append((pos,xp,x,yp,y))
+            cv2.rectangle(self.frame, (xp, yp), (x,y), (0, 255, 255), 1)
+            print "#","rectangle: ", ((xp, yp), (x,y))
+            
+        elif event == cv2.EVENT_LBUTTONDOWN:
+            self.mouse_event_note["LBUTTONDOWN"]=(x,y)
+            self.mouse_event_note["direction"]=self.direction
+            self.mouse_event_note["frame"]=self.frame
+            self.direction=0
+        elif  event == cv2.EVENT_MOUSEMOVE:
+            if "LBUTTONDOWN" in self.mouse_event_note:
+                (xp,yp)=self.mouse_event_note["LBUTTONDOWN"]
+                self.frame=numpy.copy(self.mouse_event_note["frame"])
+                cv2.rectangle(self.frame, (xp, yp), (x,y), (0, 0, 255), 1)
+
+    def select_point_by_mouse(self,event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONUP:
+#            self.direction=self.mouse_event_note["direction"]
+            self.frame=numpy.copy(self.mouse_event_note["frame"])
+            (xp,yp)=self.mouse_event_note["LBUTTONDOWN"]
+            self.mouse_event_note={}
+            cv2.rectangle(self.frame, (xp-1, yp-1), (xp+1,yp+1), (255,255, 255), 2)
+            
+        elif event == cv2.EVENT_LBUTTONDOWN:
+            self.mouse_event_note["LBUTTONDOWN"]=(x,y)
+            self.mouse_event_note["direction"]=self.direction
+            self.mouse_event_note["frame"]=numpy.copy(self.frame)
+            cv2.rectangle(self.frame, (x-1, y-1), (x+1,y+1), (255,0, 255), 1)
+            self.direction=0
+            print "#","point: ", (x,y)
+            pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)-1
+            self.inputted_data.append((pos,x,y))
+
+    def get_mask(self,frame):
+        if self.bg==[]:
+            return None
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        bg = self.bg[-1]
+        bg = cv2.cvtColor(bg, cv2.COLOR_BGR2HSV)
+        temp =cv2.absdiff(img,bg)
+        temp = cv2.threshold(temp, 15, 255, cv2.THRESH_BINARY)[1]
+        operator = numpy.ones((3, 3), numpy.uint8)
+        temp = cv2.dilate(temp, operator, iterations=4)
+        temp = cv2.erode(temp, operator, iterations=4)
+        temp = cv2.dilate(temp, operator, iterations=4)
+        temp = cv2.erode(temp, operator, iterations=4)
+        temp[:,:,1]=temp[:,:,0]
+        temp[:,:,2]=temp[:,:,0]
+        mask = temp
+        return mask
+
+    def update_frame(self,pos=None):
+        if not pos is None:
+            if pos>0:
+                self.original.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos)
+            else:
+                self.direction=0
+                self.original.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos)
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos
+                print "#"," ! This is the first frame."                
+        (ret, frame) = self.original.read()
+        if ret:
+            self.frame = frame
+            if self.currentstep>100:
+                mask=self.get_mask(frame)
+                if not mask is None:
+                    #self.frame=cv2.bitwise_and(frame,mask)                
+                    self.frame=cv2.bitwise_or(frame,cv2.bitwise_not(mask))
+            if self.currentstep>200:
+                template = self.template[-1]
+                gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+                s=res.shape
+                hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+                ss=hsv.shape
+                d0=(ss[0]-s[0])//2
+                d1=(ss[1]-s[1])//2
+#                hsv[d0:s[0]+d0,d1:s[1]+d1,0]=90*res
+#                hsv[d0:s[0]+d0,d1:s[1]+d1,1]=255*res
+                hsv[d0:s[0]+d0,d1:s[1]+d1,2]=255*res
+                self.frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+                if not mask is None:
+                    self.frame=cv2.bitwise_or(self.frame,cv2.bitwise_not(mask))
+#                    self.frame=cv2.bitwise_and(self.frame,mask)
+                self.frame=cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        else:
+            self.direction=0
+            pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+            print "#","frameposition", pos
+            print "#"," ! This is the final frame."
+            
+        
+    def showvideo(self):
+        print "#","q|c: quit;"
+        print "#","f: forward;"
+        print "#","b: reverse;"
+        print "#","n: pause at next frame;"
+        print "#","p: pause at previous frame;"
+        print "#","e: pause at final frame;"
+        print "#","a: pause at first frame;"
+        print "#","]: next step"
+        while(self.original.isOpened()):
+            if self.direction>0:
+                self.update_frame()
+            elif self.direction<0:
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                pos=pos-2
+                self.update_frame(pos)
+
+            cv2.imshow('frame',self.frame)
+            keyinput=cv2.waitKey(1) & 0xFF
+            if keyinput == ord('q'):
+                print "#","break"
+                break
+            elif keyinput == ord('c'):
+                print "#","break"
+                break
+            elif keyinput == ord('p'):
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                pos=pos-2
+                self.update_frame(pos)
+                self.direction=0
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos
+            elif keyinput == ord('n'):
+                self.update_frame()
+                self.direction=0
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos
+            elif keyinput == ord('e'):
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)-1
+                self.update_frame(pos)
+                self.direction=0
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos
+            elif keyinput == ord('a'):
+                pos=0
+                self.update_frame(pos)
+                self.direction=0
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos
+            elif keyinput == ord('f'):
+                self.direction=1
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos, "->"
+            elif keyinput == ord('b'):
+                self.direction=-1
+                pos=self.original.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+                print "#","frameposition", pos, "<-"
+            elif keyinput == ord(']'):
+                self.direction=0
+                return
+            elif keyinput == ord('x'):
+                print "#","x:"
+                if not self.event_x is None:
+                    self.event_x()
+
+
+
+if __name__=="__main__":
+    original=cv2.VideoCapture('a.avi')
+    tf=TrackingFilter(original)
+    tf.run()
+    original.release()
+    cv2.destroyAllWindows()
